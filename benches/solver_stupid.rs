c7b068ef31499d6e8aed4784129b02250a2bc6fa
PlexSheep <software@cscherr.de>
2024-08-07 22:10:17 +0200

    feat(bench): move benching to actual benches, primitively

---
+++ benches/solver_stupid.rs
@@ -0,0 +1,19 @@
+use wordle_analyzer::bench::builtin::BuiltinBenchmark;
+use wordle_analyzer::bench::Benchmark;
+use wordle_analyzer::game::{self, GameBuilder};
+use wordle_analyzer::solve::Solver;
+use wordle_analyzer::solve::StupidSolver;
+use wordle_analyzer::wlist::builtin::BuiltinWList;
+
+fn main() -> anyhow::Result<()> {
+    let wl = BuiltinWList::english(5);
+    let builder: GameBuilder<'_, BuiltinWList> = game::Game::builder(&wl)
+        .length(5)
+        .max_steps(6)
+        .precompute(true);
+    let solver: StupidSolver<_> = StupidSolver::build(&wl)?;
+    let bench = BuiltinBenchmark::build(&wl, solver, builder, 16)?;
+    bench.start(2000, &bench.builder())?;
+    println!("{}", bench.report());
+    Ok(())
+}
